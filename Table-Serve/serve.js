a470caf7144bd7eaa2d2deae1f7006be99e8bdef
Ravi Kumar <you@example.com>
2020-10-13 14:33:20 +0530

    Add Filter api

--- Table-Serve/serve.js
+++ Table-Serve/serve.js
@@ -2,31 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require('fs');
-const playstore = require('./playstore'); 
-
+const playstore = require('./playstore');
 const PORT = 8000;
-
 const app = express();
-
 app.use(bodyParser.json());
-
-const mongoose =require('mongoose')
-
+const mongoose = require('mongoose')
 app.use(cors());
 
-
-
-
-mongoose.connect('mongodb://localhost:27017/playstore', {useNewUrlParser: true,useUnifiedTopology: true},(error)=>{
-    if(!error){
+mongoose.connect('mongodb://localhost:27017/playstore', { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+    if (!error) {
         console.log("Connection Success");
     }
-    else{
+    else {
         console.log("connection Not success");
     }
 });
 
-const sendappdata=(res)=>{
+const sendappdata = (res) => {
     fs.readFile('./_file/data.json', (err, data) => {
         if (err) throw err;
         let appdata = JSON.parse(data);
@@ -41,41 +33,31 @@ const sendappdata=(res)=>{
     })
 }
 
-
 app.listen(PORT, function () {
-  console.log("Server running at...", PORT);
+    console.log("Server running at...", PORT);
 });
 
-app.get('/show',async(req,res)=>{
+app.get('/show', async (req, res) => {
     console.log('server hit')
     sendappdata(res);
     // res.send(appdata)
-   });
+});
 
-
-app.post('/getplaystoredata', (req,res) =>{
+app.post('/getplaystoredata', (req, res) => {
     console.log(req.body)
-    const skiprecord =req.body.skip;
-
-    // const tempIndex=(pageindex-1)*10;
-    // const pageLimit=10;
-    const conditionalStatement=req.body.filter.Category ? req.body.filter:{}
-    if(req.body.filter.Category)
-    {
-    playstore.find(req.body.filter).skip(skiprecord).limit(10).then((data)=>{console.log('data======>>>>>>>>>.',data.length)
-    res.send(data)    
-});
+    const skiprecord = req.body.skip;
+    if (req.body.filter) {
+        playstore.find(req.body.filter).skip(skiprecord).limit(10).then((data) => {
+            console.log('data======>>>>>>>>>.', data.length)
+            res.send(data)
+        });
     }
-    else{
-        playstore.find({}).skip(skiprecord).limit(10).then((data)=>{console.log('data======>>>>>>>>>.',data.length)
-    res.send(data)    
-});
+    else {
+        playstore.find({}).skip(skiprecord).limit(10).then((data) => {
+            console.log('data======>>>>>>>>>.', data.length)
+            res.send(data)
+        });
     }
-    // playstore.find({},{skip:0, limit: 10},function(err, results) {
-    //     res.send(results);
-    //     console.log(results)
-    // });
-
 })
 
 
